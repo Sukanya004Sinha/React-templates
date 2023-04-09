3215fafc4ab23961d1b508e468ee2bda7755a95d
Sukanya Sinha <53162459+Sukanya004Sinha@users.noreply.github.com>
2023-04-09 19:13:47 +0530

    Add files via upload
    completed upto pagination design

--- imbd/src/App.js
+++ imbd/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Banner from './components/Banner';
 import NavBar from './components/NavBar';
+import Movies from "./components/Movies"
+import Pagination from './components/Pagination';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <h1> Hello Sukanya</h1>
     <NavBar>test</NavBar>
     <Banner></Banner>
+    <Movies></Movies>
+    <Pagination></Pagination>
     {/* NavBar
     * Banner
     * Movies
